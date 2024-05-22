4ec8fcc9d78c88fa4f772dc322d5564406d427c8
alex <alex.zeiss@gmx.net>
2024-05-22 11:20:18 +0200

    Add 02 functions

---
+++ js/02-functions.js
@@ -0,0 +1,16 @@
+/** 02 Executing Functions Directly & Indirectly */
+function double(n) {
+  return n * 2;
+}
+
+function transform(n, fn) {
+  return fn(n);
+}
+
+console.log(transform(2, double));
+
+console.log(
+  transform(4, function (n) {
+    return n * 2;
+  })
+);
